f49a47aaa109c45ff287373c7967a5b339e9e2a1
celer <fractal@fractal.com.pe>
2023-04-25 03:59:39 -0500

    build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // import VPannellum from 'vue-pannellum'
 export default defineNuxtConfig({
+	ssr: false,
 	modules: [
 		'nuxt-quasar-ui'
 	],
